09273933088cda7020b09d508c21d1f15145e75b
fede <fedevargas110@gmail.com>
2019-05-28 10:36:48 -0300

    saving instances

--- FGZ/serializers.py
+++ FGZ/serializers.py
@@ -5,7 +5,16 @@ class AnimalSerializer(serializers.ModelSerializer):
   class Meta:
     model = Animal
     fields = '__all__'
+
   def to_representation(self, instance):
     ndeah = super().to_representation(instance)
     ndeah['gender'] = instance.get_gender_display()
     return ndeah
+
+  def create(self, validated_data):
+    return Animal.objects.create(**validated_data)
+
+  def update(self, instance, validated_data):
+    instance.id_animal = validated_data.get('ID', instance.id_animal)
+    instance.save()
+    return instance

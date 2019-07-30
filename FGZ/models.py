dbb63b607bad8afe6b40fdc32f6eb1166980d83e
fede <fedevargas110@gmail.com>
2019-07-30 10:54:17 -0300

    cap en animals

--- FGZ/models.py
+++ FGZ/models.py
@@ -13,6 +13,16 @@ TIPO = (
   (1, 'Gasto'),
 )
 
+class CAP(models.Model):
+  id_cap = models.AutoField(primary_key=True)
+  nameC = models.CharField(max_length=20)
+  last_nameC = models.CharField(max_length=20)
+  age = models.CharField(max_length=20)
+  address = models.CharField(max_length=120)
+  
+  def __str__(self):
+    return str(self.id_cap) + ' ' + str(self.nameC)
+
 class Animal(models.Model):
   id_animal = models.AutoField(primary_key=True)
   name = models.CharField(max_length=20, blank=True, null=True)
@@ -22,17 +32,10 @@ class Animal(models.Model):
   photo = models.ImageField(blank=True, upload_to='img/', null=True)
   species = models.CharField(max_length=120, blank=True, null=True)
   gender = models.PositiveIntegerField(choices=GENDERS, default=0, blank=True, null=True)
+  cap = models.ForeignKey(CAP, on_delete=models.CASCADE, null=True, blank=True)
 
   def __str__(self):
     return str(self.id_animal) + ' ' + str(self.name)
-
-
-class CAP(models.Model):
-  id_cap = models.AutoField(primary_key=True)
-  nameC = models.CharField(max_length=20)
-  last_nameC = models.CharField(max_length=20)
-  age = models.CharField(max_length=20)
-  address = models.CharField(max_length=120)
 
 class Monto(models.Model):
   date = models.DateField(blank=True, null=True)
